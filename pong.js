1428226614aa0f6440bc10e7ab2864375642b948
Mohammed Arabiat <tanjoodo@gmail.com>
2016-09-19 22:08:53 +0300

    Preliminary addition of aabb. Mostly broken.

--- pong.js
+++ pong.js
@@ -24,24 +24,63 @@ function createProgram(gl, vertexShader, fragmentShader) {
 	console.log(gl.getProgramInfoLog(program));
 	gl.deleteProgram(program);
 }
+
 var gl;
 
 function drawPaddle() {
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
 	gl.useProgram(this.program);
-	resloc = gl.getUniformLocation(this.program, "res");
+	var resloc = gl.getUniformLocation(this.program, "res");
+	var trans = gl.getUniformLocation(this.program, "translation");
+	var scale = gl.getUniformLocation(this.program, "scale");
+	var color = gl.getUniformLocation(this.program, "uColor");
+
 	gl.uniform2f(resloc, res[0], res[1]);
-	trans = gl.getUniformLocation(this.program, "translation");
 	gl.uniform4f(trans, this.posx, this.posy, 0, 0);
-	scale = gl.getUniformLocation(this.program, "scale");
 	gl.uniform4f(scale, this.scalex, this.scaley, 1, 1);
+	gl.uniform4f(color, 1, 1, 1, 1);
 
 	var positionloc = gl.getAttribLocation(this.program, "a_position");
 	gl.enableVertexAttribArray(positionloc);
 	gl.vertexAttribPointer(positionloc, 2, gl.FLOAT, false, 0, 0);
 
 	gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+	if (DRAW_AABB) {
+		this.aabb.draw();
+	}
 }
+
+// magic code from another pong project
+function aabbIntersects(aabb)
+{
+	if ((this.x >= aabb.x) && (this.x <= aabb.x + aabb.w))
+	{
+		if ((this.y >= aabb.y) && (this.y <= aabb.y + aabb.h))
+			return true;
+	}
+
+	if ((this.x + this.w >= aabb.x) 
+		&& ((this.x + this.w) <= (aabb.x + aabb.w + this.w)))
+	{
+		if ((this.y + this.h >= aabb.y) && (this.y + this.h <= aabb.y + aabb.h + this.h))
+			return true;
+	}
+	return false;
+}
+
+function aabbUpdate(delta) {
+	var points = [];
+}
+
+function centerToBottomLeft(x, y, w, h) {
+	return {x:x-w/2, y:y-h/2};
+}
+
+function bottomLeftToCenter(x,y, w, h) {
+	return {x:x+w/2, y:y+h/2};
+}
+
 var UP_ARROW = 38;
 var DOWN_ARROW = 40;
 var LEFT_ARROW = 37;
@@ -50,6 +89,9 @@ var RIGHT_ARROW = 39;
 var mouseX = 0;
 var mouseY = 0;
 var mouseDown = false;
+
+var DRAW_AABB = true;
+var aabbs = [];
 
 var gameObjects = [];
 var models = [];
@@ -64,6 +106,78 @@ var di = 0;
 
 var canvasWidth;
 var canvasHeight;
+
+var testAabb;
+var testPaddle;
+
+function drawAabb() {
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+	gl.useProgram(aabb.program);
+	var resloc = gl.getUniformLocation(aabb.program, "res");
+	var trans = gl.getUniformLocation(aabb.program, "translation");
+	var scale = gl.getUniformLocation(aabb.program, "scale");
+	var color = gl.getUniformLocation(aabb.program, "uColor");
+
+	gl.uniform2f(resloc, res[0], res[1]);
+	var centerPoint = bottomLeftToCenter(this.x, this.y, this.w, this.h);
+	gl.uniform4f(trans, centerPoint.x, centerPoint.y, 0, 0);
+	gl.uniform4f(scale, this.owner.scalex, this.owner.scaley, 1, 1);
+	gl.uniform4f(color, 0, 0, 1, 1);
+
+	var positionloc = gl.getAttribLocation(aabb.program, "a_position");
+	gl.enableVertexAttribArray(positionloc);
+	gl.vertexAttribPointer(positionloc, 2, gl.FLOAT, false, 0, 0);
+
+	gl.drawArrays(gl.LINE_STRIP, 0, 4);
+
+}
+
+// owner is an object that contains these fields: posx, posy, scalex, scaley
+function aabb(x ,y, w, h, owner) {
+	var bottomLeft = centerToBottomLeft(x, y, w, h);
+	var new_x = bottomLeft.x;
+	var new_y = bottomLeft.y;
+
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	var points = [
+		new_x, new_y,
+		new_x + w, new_y,
+		new_x + w, new_y + h,
+		new_x, new_y + h
+	];
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+
+	var myInstance = {
+		x:new_x,
+		y:new_y,
+		w:w,
+		h:h,
+		buffer:buffer,
+		owner:owner,
+		update:aabbUpdate,
+		draw:drawAabb,
+		intersects:aabbIntersects
+	};
+
+	return myInstance;
+}
+
+// shader program can be set up statically to be used by all instances. Called once in start()
+function aabbSetUpStatic() {
+	var vShaderSource = document.getElementById("vshader").text;
+	var fShaderSource = document.getElementById("fshader").text;
+
+	var vShader = createShader(gl, gl.VERTEX_SHADER, vShaderSource);
+	var fShader = createShader(gl, gl.FRAGMENT_SHADER, fShaderSource);
+
+	var program = createProgram(gl, vShader, fShader);
+
+	aabb.program = program;
+	console.log(aabb.program);
+
+}
+
 function updatePaddle(delta) {
 	if (keyState[UP_ARROW] == true) {
 		this.speedy = 100/60.0;
@@ -99,16 +213,30 @@ function paddle() {
 
 	var paddleProgram = createProgram(gl, vertexShader, fragmentShader);
 
-	return {posx:30,
+	var posx = 30;
+	var posy = 0;
+	var model = models["paddle"];
+	var w = Math.abs(model[0] - model[2]);
+	var h = Math.abs(model[1] - model[3]);
+
+	myInstance = {
+		posx:posx,
 		speedx:0,
 		speedy:0,
-		posy:0,
+		posy:posy,
 		scaley:50,
 		scalex:50,
 		buffer:paddleBuffer,
 		program:paddleProgram,
 		draw:drawPaddle,
-		update:updatePaddle};
+		update:updatePaddle
+	};
+
+	var myAabb = new aabb(posx, posy, w, h, myInstance); 
+
+	myInstance.aabb = myAabb;
+
+	return myInstance;
 
 }
 
@@ -204,6 +332,8 @@ function start() {
 
 	resize();
 	console.log(canvasHeight);
+	aabbSetUpStatic();
+	gl.lineWidth(5);
 
 	requestAnimationFrame(tick);
 
